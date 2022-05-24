d6d5538ad2c81ba172f0fcf457000ebab25693e5
Jared Henderson <jaredh159@users.noreply.github.com>
2022-05-24 14:22:46 -0400

    test deploys

--- components/src/shared/Button/Button.tsx
+++ components/src/shared/Button/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button: React.FC = () => {
   return (
     <button className="p-4 text-xs rounded-full text-white bg-gertie">
-      Buttons are neat.
+      Buttons are TEST 3 DEPLOYS.
     </button>
   );
 };
