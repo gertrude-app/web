c82ed4d52c280ca4487e0a575a671e13bb4d0e8f
Miciah Henderson <miciahjohnhenderson@gmail.com>
2022-06-28 11:27:35 -0400

    created waitlist joining ui and extracted out Button

--- components/src/shared/Button/Button.stories.tsx
+++ components/src/shared/Button/Button.stories.tsx
@@ -9,4 +9,34 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const PrimaryViolet = () => (
+  <div className="p-20">
+    <Button type="primary-violet" onClick={() => {}}>
+      Join the waitlist
+    </Button>
+  </div>
+);
+
+export const PrimaryWhite = () => (
+  <div className="p-20 bg-violet-500">
+    <Button type="primary-white" onClick={() => {}}>
+      Join the waitlist
+    </Button>
+  </div>
+);
+
+export const SecondaryWhite = () => (
+  <div className="p-20">
+    <Button type="secondary-white" onClick={() => {}}>
+      Cancel
+    </Button>
+  </div>
+);
+
+export const SecondaryViolet = () => (
+  <div className="p-20 bg-violet-500">
+    <Button type="secondary-violet" onClick={() => {}}>
+      Join the waitlist
+    </Button>
+  </div>
+);
--- components/src/shared/Button/Button.tsx
+++ components/src/shared/Button/Button.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const Button: React.FC = () => {
+interface Props {
+  className?: string;
+  onClick(): void;
+  type: 'primary-violet' | 'primary-white' | 'secondary-violet' | 'secondary-white';
+  children: string;
+}
+
+const Button: React.FC<Props> = ({ className, onClick, type, children }) => {
+  let colors = ``;
+  switch (type) {
+    case 'primary-violet':
+      colors = `bg-violet-500 text-white hover:bg-violet-600 border-2 border-violet-500 hover:border-violet-600 ring ring-white ring-offset-0 focus:ring-offset-4 focus:ring-violet-500`;
+      break;
+    case 'primary-white':
+      colors = `bg-white text-violet-500 hover:bg-violet-50 border-2 border-white hover:border-violet-50 ring ring-violet-500 ring-offset-0 ring-offset-violet-500 focus:ring-offset-4 focus:ring-white`;
+      break;
+    case 'secondary-violet':
+      colors = `bg-violet-500 text-white border-2 border-white hover:bg-violet-400 ring ring-violet-500 ring-offset-0 focus:ring-offset-4 focus:ring-white ring-offset-violet-500`;
+      break;
+    case 'secondary-white':
+      colors = `bg-gray-50 text-gray-500 border hover:bg-gray-100 ring ring-white ring-offset-0 focus:ring-offset-4 focus:ring-indigo-400 focus:border-indigo-500`;
+      break;
+  }
+
   return (
-    <button className="p-4 text-xs rounded-full text-white bg-gertie">
-      Buttons are neat.
+    <button
+      className={`${colors} py-2.5 px-10 shadow-sm rounded-lg text-lg font-bold [transition:100ms] outline-none ${className}`}
+      onClick={onClick}
+    >
+      {children}
     </button>
   );
 };
