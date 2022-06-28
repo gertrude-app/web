676a2e4453811d372f75b52fe7dae572c04a969d
Jared Henderson <jaredh159@users.noreply.github.com>
2022-06-28 15:09:28 -0400

    reduce duplication in Button component

--- components/src/shared/Button/Button.tsx
+++ components/src/shared/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cx from 'classnames';
 
 interface CommonProps {
   className?: string;
@@ -11,38 +12,38 @@ type Props =
   | ({ type: 'button'; onClick(): void } & CommonProps)
   | ({ type: 'link'; to: string } & CommonProps);
 
-const Button: React.FC<Props> = (props) => {
+const Button: React.FC<Props> = ({ color, className, ...props }) => {
   let colors = ``;
-  switch (props.color) {
+  switch (color) {
     case `primary-violet`:
-      colors = `bg-violet-500 text-white hover:bg-violet-600 border-2 border-violet-500 hover:border-violet-600 ring ring-white ring-offset-0 focus:ring-offset-4 focus:ring-violet-500`;
+      colors = `bg-violet-500 text-white hover:bg-violet-600 border-2 border-violet-500 hover:border-violet-600 ring-white focus:ring-violet-500`;
       break;
     case `primary-white`:
-      colors = `bg-white text-violet-500 hover:bg-violet-50 border-2 border-white hover:border-violet-50 ring ring-violet-500 ring-offset-0 ring-offset-violet-500 focus:ring-offset-4 focus:ring-white`;
+      colors = `bg-white text-violet-500 hover:bg-violet-50 border-2 border-white hover:border-violet-50 ring-violet-500 ring-offset-violet-500 focus:ring-white`;
       break;
     case `secondary-violet`:
-      colors = `bg-violet-500 text-white border-2 border-white hover:bg-violet-400 ring ring-violet-500 ring-offset-0 focus:ring-offset-4 focus:ring-white ring-offset-violet-500`;
+      colors = `bg-violet-500 text-white border-2 border-white hover:bg-violet-400 ring-violet-500 focus:ring-white ring-offset-violet-500`;
       break;
     case `secondary-white`:
-      colors = `bg-gray-50 text-gray-500 border hover:bg-gray-100 ring ring-white ring-offset-0 focus:ring-offset-4 focus:ring-indigo-400 focus:border-indigo-500`;
+      colors = `bg-gray-50 text-gray-500 border hover:bg-gray-100 ring-white focus:ring-indigo-400 focus:border-indigo-500`;
       break;
   }
+  const classes = cx(
+    colors,
+    `ring ring-offset-0 focus:ring-offset-4 py-2.5 px-10 shadow-sm rounded-lg text-lg font-bold [transition:100ms] outline-none block`,
+    className,
+    props.type !== `button` && `text-center w-fit`,
+  );
 
   if (props.type === `button`) {
     return (
-      <button
-        className={`${colors} py-2.5 px-10 shadow-sm rounded-lg text-lg font-bold [transition:100ms] outline-none block ${props.className}`}
-        onClick={props.onClick}
-      >
+      <button className={classes} onClick={props.onClick}>
         {props.children}
       </button>
     );
   }
   return (
-    <Link
-      className={`${colors} py-2.5 px-10 shadow-sm rounded-lg text-lg font-bold [transition:100ms] outline-none block w-fit text-center ${props.className}`}
-      to={props.to}
-    >
+    <Link className={classes} to={props.to}>
       {props.children}
     </Link>
   );
