d73ec9386b4ddb47413e72fc93f387ded1a7e691
Miciah Henderson <miciahjohnhenderson@gmail.com>
2022-07-04 18:19:02 -0400

    made DashboardPageHeading component

---
+++ components/src/shared/DashboardPageHeading/DashboardPageHeading.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import DashboardPageHeading from './DashboardPageHeading';
+
+export default {
+  title: 'DashboardPageHeading',
+  component: DashboardPageHeading,
+} as ComponentMeta<typeof DashboardPageHeading>;
+
+const Template: ComponentStory<typeof DashboardPageHeading> = (args) => (
+  <DashboardPageHeading {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Profile',
+  icon: 'user',
+};
--- dashboard/src/components/routes/Profile.tsx
+++ dashboard/src/components/routes/Profile.tsx
@@ -1,10 +1,14 @@
+import DashboardPageHeading from '@shared/DashboardPageHeading';
+import TextInput from '@shared/TextInput';
 import React from 'react';
 import DashboardChrome from '../DashboardChrome';
 
 const Profile: React.FC = () => {
   return (
     <DashboardChrome>
-      <h1>profile</h1>
+      <div className="py-10 px-20">
+        <DashboardPageHeading icon="user">Profile</DashboardPageHeading>
+      </div>
     </DashboardChrome>
   );
 };
