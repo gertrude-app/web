2ac4e902856b5680ad31888d9744005211a9eb41
Jared Henderson <jaredh159@users.noreply.github.com>
2022-07-04 16:43:30 -0400

    hook up marketing 'join waitlist' buttons to dashboard screen

--- marketing/components/MainHeader.tsx
+++ marketing/components/MainHeader.tsx
@@ -6,7 +6,12 @@ const MainHeader: React.FC = () => (
   <header className="flex justify-between items-center px-5 sm:px-8 py-6 border-b-2 border-white border-opacity-20 bg-violet-500">
     <Logo type="inverted" />
     <div>
-      <Button type="button" onClick={() => {}} color="primary-white" small>
+      <Button
+        type="external"
+        href={process.env.NEXT_PUBLIC_JOIN_WAITLIST_URL ?? ``}
+        color="primary-white"
+        small
+      >
         Sign up
       </Button>
     </div>
