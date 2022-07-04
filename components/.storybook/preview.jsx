efa34483c94fdfe20b8d8b8e706a928b7608579c
Jared Henderson <jaredh159@users.noreply.github.com>
2022-07-04 09:53:19 -0400

    tweak storybook viewports, 375px is our smallest target

--- components/.storybook/preview.jsx
+++ components/.storybook/preview.jsx
@@ -4,8 +4,26 @@ import { MemoryRouter } from 'react-router-dom';
 
 addDecorator((storyFn) => <MemoryRouter>{storyFn()}</MemoryRouter>);
 
+const viewports = {
+  smallMobile: {
+    name: 'Small mobile',
+    styles: {
+      width: '375px',
+      height: '812px',
+    },
+  },
+  largeMobile: {
+    name: 'Large mobile',
+    styles: {
+      width: '428px',
+      height: '926px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: { viewports },
   controls: {
     matchers: {
       color: /(background|color)$/i,
