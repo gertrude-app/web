4f1af2d25cb83138f5bfa539b35c75d7c3d8c062
Jared Henderson <jaredh159@users.noreply.github.com>
2022-07-04 16:19:34 -0400

    add more flexibility to Logo rendering

--- components/src/shared/Logo/Logo.stories.tsx
+++ components/src/shared/Logo/Logo.stories.tsx
@@ -5,8 +5,53 @@ import Logo from './Logo';
 export default {
   title: `Logo`,
   component: Logo,
+  parameters: { layout: `fullscreen` },
 } as ComponentMeta<typeof Logo>;
 
 const Template: ComponentStory<typeof Logo> = (args) => <Logo {...args} />;
 
 export const Default = Template.bind({});
+Default.args = { type: `default`, size: 45 };
+Default.decorators = [
+  (Story) => (
+    <div className="py-8 flex justify-center">
+      <Story />
+    </div>
+  ),
+];
+
+export const DefaultIconOnly = Template.bind({});
+DefaultIconOnly.args = {
+  type: `default`,
+  iconOnly: true,
+  size: 75,
+};
+DefaultIconOnly.decorators = [
+  (Story) => (
+    <div className="py-8 flex justify-center">
+      <Story />
+    </div>
+  ),
+];
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+  type: `inverted`,
+  iconOnly: false,
+  size: 40,
+};
+Inverted.decorators = [
+  (Story) => (
+    <div className="py-8 bg-violet-500 flex justify-center">
+      <Story />
+    </div>
+  ),
+];
+
+export const InvertedIconOnly = Template.bind({});
+InvertedIconOnly.decorators = Inverted.decorators;
+InvertedIconOnly.args = { ...Inverted.args, iconOnly: true };
+
+export const InvertedLarge = Template.bind({});
+InvertedLarge.decorators = Inverted.decorators;
+InvertedLarge.args = { ...Inverted.args, iconOnly: true, size: 65 };
