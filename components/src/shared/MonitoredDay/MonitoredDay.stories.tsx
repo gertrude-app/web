900200a452f3b033f15dab502cceed46ad9b2446
Kiah <miciahjohnhenderson@gmail.com>
2022-07-30 14:00:08 -0400

    fixed story for MoniteredDate

--- components/src/shared/MonitoredDay/MonitoredDay.stories.tsx
+++ components/src/shared/MonitoredDay/MonitoredDay.stories.tsx
@@ -7,6 +7,9 @@ export default {
   component: MonitoredDay,
 } as ComponentMeta<typeof MonitoredDay>;
 
-const Template: ComponentStory<typeof MonitoredDay> = (args) => <MonitoredDay {...args} />;
+const Template: ComponentStory<typeof MonitoredDay> = (args) => (
+  <MonitoredDay {...args} />
+);
 
 export const Default = Template.bind({});
+Default.args = { date: new Date(), numItems: 36, numCompleted: 24, to: '/' };
