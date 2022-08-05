d34bb3e26874247508b60ab6e2ef6a2f48620797
Kiah <miciahjohnhenderson@gmail.com>
2022-08-04 21:56:14 -0400

    made stories for ProtectedUserCard

--- components/src/shared/ProtectedUserCard/ProtectedUserCard.stories.tsx
+++ components/src/shared/ProtectedUserCard/ProtectedUserCard.stories.tsx
@@ -7,6 +7,40 @@ export default {
   component: ProtectedUserCard,
 } as ComponentMeta<typeof ProtectedUserCard>;
 
-const Template: ComponentStory<typeof ProtectedUserCard> = (args) => <ProtectedUserCard {...args} />;
+const Template: ComponentStory<typeof ProtectedUserCard> = (args) => (
+  <ProtectedUserCard {...args} />
+);
 
-export const Default = Template.bind({});
+export const Standard = Template.bind({});
+Standard.args = {
+  devices: [{ title: '14" Macbook Pro', status: 'online' }],
+  title: 'John Doe',
+  keychains: 4,
+  keys: 57,
+  screenshots: true,
+  keystrokes: false,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  devices: [],
+  title: 'John Doe',
+  keychains: 0,
+  keys: 0,
+  screenshots: false,
+  keystrokes: false,
+};
+
+export const Full = Template.bind({});
+Full.args = {
+  devices: [
+    { title: '13" MacBook Air', status: 'online' },
+    { title: 'Mac Mini', status: 'offline' },
+    { title: 'iMac', status: 'offline' },
+  ],
+  title: 'John Doe',
+  keychains: 32,
+  keys: 1546,
+  screenshots: true,
+  keystrokes: true,
+};
