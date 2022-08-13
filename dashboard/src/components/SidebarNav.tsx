cee5b536dbbe8057a85e57a117ad12c2f5580bf2
Kiah <miciahjohnhenderson@gmail.com>
2022-08-13 09:11:04 -0400

    dash: updated NotificationCard

---
+++ dashboard/src/components/SidebarNav.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Logo from '@shared/Logo';
+import SidebarOption from './SidebarOption';
+
+interface Props {
+  expanded: boolean;
+  setExpanded(expanded: boolean): void;
+  open: boolean;
+  setOpen(open: boolean): void;
+}
+
+const SidebarNav: React.FC<Props> = ({ expanded, setExpanded, open, setOpen }) => (
+  <nav
+    className={`h-screen fixed bg-gray-900 bg-gradient-to-br from-transparent to-violet-900/40 -ml-80 z-40 ${
+      open ? (expanded ? `left-80` : `left-20`) : `left-0`
+    } lg:-ml-0 flex flex-col justify-between items-start overflow-hidden [transition:200ms] ${
+      expanded ? `w-72` : `w-20`
+    }`}
+  >
+    <div
+      className="absolute lg:hidden right-0 top-0 p-4 cursor-pointer text-white text-opacity-50 hover:text-opacity-60"
+      onClick={() => setOpen(false)}
+    >
+      <i className="fa fa-times text-2xl transition duration-100" />
+    </div>
+    <div className={`${expanded ? `p-4` : `py-4`}`}>
+      <Logo
+        type="on-dark"
+        className={`m-2 mb-10 mt-8 ${expanded ? `ml-4` : `ml-[1.2em]`}`}
+        iconOnly={!expanded}
+      />
+      <a
+        className={`text-white text-opacity-50 text-right absolute bottom-0 left-44 -ml-1 py-5 hover:text-opacity-60 transition duration-100 w-32 justify-center items-center cursor-pointer ${
+          expanded ? `flex` : `hidden`
+        }`}
+        href="https://friendslibrary.com"
+      >
+        Log out <i className="ml-1.5 fa fa-sign-out" />
+      </a>
+      <div className="mt-5 pb-4">
+        <SidebarOption icon="home" expanded={expanded} to="/">
+          Dashboard
+        </SidebarOption>
+        <SidebarOption icon="shield" expanded={expanded} to="/digital-guardians">
+          Digital guardians
+        </SidebarOption>
+        <SidebarOption icon="users" expanded={expanded} to="/guarded-users">
+          Guarded users
+        </SidebarOption>
+        <SidebarOption icon="key" expanded={expanded} to="/keychains">
+          Keychains
+        </SidebarOption>
+        <SidebarOption icon="user" expanded={expanded} to="/profile">
+          Profile
+        </SidebarOption>
+      </div>
+      <div className="border-t border-white border-opacity-10 pt-4 -ml-4 pl-4">
+        <SidebarOption icon="life-ring" expanded={expanded} to="/support">
+          Support
+        </SidebarOption>
+        <SidebarOption icon="cog" expanded={expanded} to="/settings">
+          Settings
+        </SidebarOption>
+      </div>
+    </div>
+    <div
+      className={`items-center justify-start px-6 w-80 ${
+        expanded ? `py-5 px-6` : `h-16 pl-8`
+      } self-stretch bg-black bg-opacity-0 hover:bg-opacity-10 cursor-pointer transition duration-100 hidden lg:flex`}
+      onClick={() => setExpanded(!expanded)}
+    >
+      <i
+        className={`fas fa-chevron-${
+          expanded ? `left mr-3` : `right`
+        } text-2xl text-white text-opacity-60`}
+      />
+    </div>
+  </nav>
+);
+
+export default SidebarNav;
--- dashboard/src/components/routes/Profile.tsx
+++ dashboard/src/components/routes/Profile.tsx
@@ -1,62 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import Button from '@shared/Button';
 import PillBadge from '@shared/dashboard/PillBadge';
 import PageHeading from '@shared/dashboard/PageHeading';
 import NotificationCard from '@shared/dashboard/NotificationCard';
 import TextInput from '@shared/TextInput';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Chrome from '../Chrome';
 
-const Profile: React.FC = () => (
-  <div className="py-10 px-5 sm:px-10 lg:px-12 xl:px-16">
-    <PageHeading icon="user">Profile</PageHeading>
-    <div className="flex flex-col md:flex-row mt-8">
-      <div className="p-8 bg-gray-100 rounded-xl flex-grow md:mr-2 border max-w-3xl">
-        <h2 className="text-lg text-gray-900 mb-2">Email address:</h2>
-        <TextInput
-          type={`email`}
-          label={``}
-          value={`johndoe@example.com`}
-          setValue={() => {}}
-        />
-      </div>
-      <div className="px-8 py-4 bg-gray-100 rounded-xl md:ml-2 flex justify-between relative border mt-4 md:mt-0">
-        <div className="flex justify-end items-start flex-col mr-8">
-          <h2 className="font-bold text-gray-700">Basic plan</h2>
-          <h3>
-            <span className="text-gray-600">$</span>
-            <span className="text-gray-700 text-3xl font-bold">10</span>
-            <span className="text-gray-600 text-lg">/month</span>
-          </h3>
-          <Link
-            className="mt-1 text-sm text-blue-700 hover:text-blue-800 cursor-pointer transition duration-100"
-            to="/profile"
-          >
-            Manage subscription
-          </Link>
+const Profile: React.FC = () => {
+  return (
+    <Chrome>
+      <div className="py-10 px-5 sm:px-10 lg:px-12 xl:px-16">
+        <PageHeading icon="user">Profile</PageHeading>
+        <div className="flex flex-col md:flex-row mt-8">
+          <div className="p-8 bg-gray-100 rounded-xl flex-grow md:mr-2 border max-w-3xl">
+            <h2 className="text-lg text-gray-900 mb-2">Email address:</h2>
+            <TextInput
+              type={`email`}
+              label={``}
+              value={`johndoe@example.com`}
+              setValue={() => {}}
+            />
+          </div>
+          <div className="px-8 py-4 bg-gray-100 rounded-xl md:ml-2 flex justify-between relative border mt-4 md:mt-0">
+            <div className="flex justify-end items-start flex-col mr-8">
+              <h2 className="font-bold text-gray-700">Basic plan</h2>
+              <h3>
+                <span className="text-gray-600">$</span>
+                <span className="text-gray-700 text-3xl font-bold">10</span>
+                <span className="text-gray-600 text-lg">/month</span>
+              </h3>
+              <Link
+                className="mt-1 text-sm text-blue-700 hover:text-blue-800 cursor-pointer transition duration-100"
+                to="/profile"
+              >
+                Manage subscription
+              </Link>
+            </div>
+            <PillBadge type="ok" className="absolute right-2 top-2">
+              active
+            </PillBadge>
+          </div>
         </div>
-        <PillBadge type="ok" className="absolute right-2 top-2">
-          active
-        </PillBadge>
+        <div className="mt-12">
+          <div className="">
+            <h2 className="text-2xl font-bold text-gray-800">Notifications</h2>
+            <div className="flex flex-wrap items-start pt-4 sm:pt-2">
+              <NotificationCard
+                method="email"
+                email="me@example.com"
+                when="suspension requests"
+              />
+              <NotificationCard
+                method="slack"
+                botToken="abc123-def456-ghi789"
+                channelId="8xjd82-jkd8j2-lkd8j2"
+                channelName="#Gertrude"
+                when="unlock requests"
+              />
+              <NotificationCard
+                method="text"
+                number="(123) 456-7890"
+                when="suspension requests"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="mt-12">
-      <h1 className="text-2xl font-bold">Notifications</h1>
-      <div className="flex flex-col">
-        <NotificationCard />
-        <NotificationCard />
-        <NotificationCard />
-        <Button
-          color="primary-violet"
-          type="button"
-          onClick={() => {}}
-          className="self-center mt-8"
-        >
-          <i className="fa fa-plus mr-4" />
-          New notification
-        </Button>
-      </div>
-    </div>
-  </div>
-);
+    </Chrome>
+  );
+};
 
 export default Profile;
