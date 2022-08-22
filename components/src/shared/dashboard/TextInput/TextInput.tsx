4a84e1ba7780f7e93981546d3257db170edc213c
Kiah <miciahjohnhenderson@gmail.com>
2022-08-22 12:34:31 -0400

    dash: added support for units in TextInput

--- components/src/shared/dashboard/TextInput/TextInput.tsx
+++ components/src/shared/dashboard/TextInput/TextInput.tsx
@@ -10,6 +10,7 @@ type Props = {
   required?: boolean;
   placeholder?: string;
   className?: string;
+  unit?: string;
 };
 
 const TextInput: React.FC<Props> = ({
@@ -21,23 +22,33 @@ const TextInput: React.FC<Props> = ({
   autoFocus = false,
   placeholder,
   className,
+  unit,
 }) => {
   const id = useId();
   return (
     <div className={cx(`flex flex-col space-y-1 w-full`, className)}>
-      <label htmlFor={id} className="text-gray-500 text-lg">
+      <label htmlFor={id} className="text-gray-500 font-semibold text-md">
         {label}
       </label>
-      <input
-        id={id}
-        type={type}
-        value={value}
-        required={!!required}
-        autoFocus={autoFocus}
-        placeholder={placeholder}
-        onChange={(e) => setValue(e.target.value)}
-        className="h-10 border ring-0 ring-gray-200 rounded-lg shadow-sm outline-none py-6 px-4 focus:shadow-md transition duration-150 focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1 text-gray-600"
-      />
+      <div className="flex">
+        <input
+          id={id}
+          type={type}
+          value={value}
+          required={!!required}
+          autoFocus={autoFocus}
+          placeholder={placeholder}
+          onChange={(e) => setValue(e.target.value)}
+          className={`h-10 border ring-0 ring-gray-200 rounded-lg shadow-sm outline-none py-6 px-4 focus:shadow-md transition duration-150 focus:border-indigo-500 focus:ring-indigo-500 focus:ring-1 text-gray-600 flex-grow z-10 ${
+            unit ? 'rounded-r-none' : 'rounded-r-lg'
+          }`}
+        />
+        {unit && (
+          <div className="flex justify-center items-center p-3 bg-gray-100 border border-l-0 rounded-r-lg">
+            <h3 className="text-gray-600">{unit}</h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- components/src/shared/dashboard/Toggle/Toggle.stories.tsx
+++ components/src/shared/dashboard/Toggle/Toggle.stories.tsx
@@ -3,10 +3,15 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Toggle from './Toggle';
 
 export default {
-  title: 'Toggle',
+  title: 'Dashboard/Core/Toggle',
   component: Toggle,
 } as ComponentMeta<typeof Toggle>;
 
-const Template: ComponentStory<typeof Toggle> = (args) => <Toggle {...args} />;
+const Template: ComponentStory<typeof Toggle> = (args) => (
+  <div className="flex flex-col space-y-4">
+    <Toggle {...args} enabled={true} />
+    <Toggle {...args} enabled={false} />
+  </div>
+);
 
 export const Default = Template.bind({});
