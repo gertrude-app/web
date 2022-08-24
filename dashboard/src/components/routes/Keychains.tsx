bd6441a717737df9be91dee77314962759e5143e
Kiah <miciahjohnhenderson@gmail.com>
2022-08-24 12:58:29 -0400

    dash: set up /keychains route

---
+++ dashboard/src/components/routes/Keychains.tsx
@@ -0,0 +1,13 @@
+import PageHeading from '@shared/dashboard/PageHeading';
+import React from 'react';
+
+const Keychains: React.FC = () => {
+  return (
+    <div className="px-4">
+      <PageHeading icon="key">Keychains</PageHeading>
+      <div></div>
+    </div>
+  );
+};
+
+export default Keychains;
