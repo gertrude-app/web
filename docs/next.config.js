69b0133c27bb4a79e14b749f8a98646dd48d0716
Jared Henderson <jaredh159@users.noreply.github.com>
2022-09-28 12:21:16 -0400

    WIP: convert to mdx

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,14 +1,27 @@
 const withMarkdoc = require('@markdoc/next.js')
+// const remarkFrontmatter = require('remark-frontmatter')
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    remarkPlugins: [require('remark-frontmatter')],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx', 'md'],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   swcMinify: true,
   experimental: {
     newNextLinkBehavior: true,
     scrollRestoration: true,
+    esmExternals: true,
   },
+  options: {},
 }
 
-module.exports = withMarkdoc()(nextConfig)
+module.exports = withMarkdoc()(withMDX(nextConfig))
