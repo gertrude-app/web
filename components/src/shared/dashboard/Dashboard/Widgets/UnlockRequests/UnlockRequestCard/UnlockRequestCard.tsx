3771b61f3ef68fa7851c854c4fcf6d3e2b620367
Jared Henderson <jaredh159@users.noreply.github.com>
2022-10-24 15:58:10 -0400

    dash: make unlock requests widget work good enough for launch

--- components/src/shared/dashboard/Dashboard/Widgets/UnlockRequests/UnlockRequestCard/UnlockRequestCard.tsx
+++ components/src/shared/dashboard/Dashboard/Widgets/UnlockRequests/UnlockRequestCard/UnlockRequestCard.tsx
@@ -3,34 +3,55 @@ import Button from '../../../../../Button';
 import { relativeTime } from '../../../../lib/dates';
 
 type Props = {
+  id: UUID;
   url: string;
   userName: string;
   comment?: string;
   createdAt: string;
 };
 
-const UnlockRequestCard: React.FC<Props> = ({ url, userName, comment, createdAt }) => (
+const UnlockRequestCard: React.FC<Props> = ({
+  id,
+  url,
+  userName,
+  comment,
+  createdAt,
+}) => (
   <div className="bg-white border p-4 rounded-xl shadow-lg mb-5">
     <div className="flex justify-between items-start mb-4">
       <h2 className="font-bold text-gray-900">{userName}</h2>
       <h3 className="text-gray-400 text-sm">{relativeTime(new Date(createdAt))}</h3>
     </div>
-    <div className="p-2 bg-violet-50 rounded-xl mt-2 flex justify-center items-center">
-      <h2 className="font-mono text-sm overflow-hidden text-ellipses">
+    <div className="py-3 px-5 bg-violet-50 rounded-xl mt-2 flex justify-center items-center">
+      <h2 className="font-mono text-base whitespace-nowrap truncate">
         {url.replace(/^https?:\/\//, ``)}
       </h2>
     </div>
     {comment && (
       <div className="flex justify-center items-center mt-3">
-        <p className="text-center text-gray-500 text-sm sm:text-base">"{comment}"</p>
+        <p className="text-center text-gray-500 text-sm sm:text-base">
+          &ldquo;{comment}&rdquo;
+        </p>
       </div>
     )}
-    <div className="flex flex-col items-stretch mt-4 w-full space-y-3">
-      <Button type="button" onClick={() => {}} color="secondary-white" small fullWidth>
+    <div className="flex flex-row space-x-3 items-stretch mt-4 w-full">
+      <Button
+        type="link"
+        to={`/unlock-requests/${id}`}
+        color="secondary-white"
+        small
+        fullWidth
+      >
         Deny
       </Button>
-      <Button type="button" fullWidth onClick={() => {}} color="primary-violet" small>
-        Create a key
+      <Button
+        type="link"
+        to={`/unlock-requests/${id}`}
+        color="primary-violet"
+        small
+        fullWidth
+      >
+        Accept &rarr;
       </Button>
     </div>
   </div>
