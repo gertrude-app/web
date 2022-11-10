445db1dd6836bb0ad7c782fd53d786ec52fe5749
Jared Henderson <jaredh159@users.noreply.github.com>
2022-11-10 15:25:14 -0500

    dash: update dashboard activity widget when items deleted

--- dash/app/src/redux/__tests__/slice-dashboard.spec.ts
+++ dash/app/src/redux/__tests__/slice-dashboard.spec.ts
@@ -2,23 +2,24 @@ import { expect, test, describe } from 'vitest';
 import { Req } from '../helpers';
 import reducer from '../slice-dashboard';
 import { acceptUnlockRequest, rejectUnlockRequest } from '../slice-unlock-requests';
+import { deleteActivityItems } from '../slice-users';
 import { makeState } from './test-helpers';
 
 describe(`reducer`, () => {
   test(`deciding unlock request removes it from dashboard`, () => {
-    const request = {
-      id: `request-123`,
-      target: `happyfish.com`,
-      userId: `2`,
-      userName: `Huck`,
-      createdAt: ``,
-    };
-
     const state = makeState();
     state.dashboard.request = {
       state: `succeeded`,
       payload: {
-        unlockRequests: [request],
+        unlockRequests: [
+          {
+            id: `request-123`,
+            target: `happyfish.com`,
+            userId: `2`,
+            userName: `Huck`,
+            createdAt: ``,
+          },
+        ],
         users: [],
         userActivity: [],
         userScreenshots: [],
@@ -33,5 +34,34 @@ describe(`reducer`, () => {
 
     next = reducer(state.dashboard, rejectUnlockRequest.succeeded(true, `request-123`));
     expect(Req.payload(next.request)?.unlockRequests).toEqual([]);
+  });
+
+  test(`approving activity items removes them from dashboard widget`, () => {
+    const state = makeState();
+    state.dashboard.request = {
+      state: `succeeded`,
+      payload: {
+        unlockRequests: [],
+        users: [],
+        userActivity: [
+          { id: `2`, userName: `Huck`, numUnreviewed: 11 },
+          { id: `3`, userName: `Bob`, numUnreviewed: 33 },
+        ],
+        userScreenshots: [],
+      },
+    };
+
+    const next = reducer(
+      state.dashboard,
+      deleteActivityItems.succeeded(true, {
+        userId: `2`,
+        itemRootIds: [`1`, `2`],
+        date: new Date(),
+      }),
+    );
+    expect(Req.payload(next.request)?.userActivity).toEqual([
+      { id: `2`, userName: `Huck`, numUnreviewed: 9 },
+      { id: `3`, userName: `Bob`, numUnreviewed: 33 },
+    ]);
   });
 });
--- dash/app/src/redux/slice-dashboard.ts
+++ dash/app/src/redux/slice-dashboard.ts
@@ -4,6 +4,7 @@ import Current from '../environment';
 import { Req } from './helpers';
 import { createResultThunk } from './thunk';
 import { acceptUnlockRequest, rejectUnlockRequest } from './slice-unlock-requests';
+import { deleteActivityItems } from './slice-users';
 
 export interface DashboardState {
   request: RequestState<DashboardWidgetData>;
@@ -29,6 +30,7 @@ export const slice = createSlice({
     builder.addCase(fetchDashboardData.succeeded, (state, action) => {
       state.request = Req.succeed(action.payload);
     });
+
     builder.addCase(rejectUnlockRequest.succeeded, (state, action) => {
       const payload = Req.payload(state.request);
       if (payload) {
@@ -37,12 +39,22 @@ export const slice = createSlice({
         );
       }
     });
+
     builder.addCase(acceptUnlockRequest.succeeded, (state, action) => {
       const payload = Req.payload(state.request);
       if (payload) {
         payload.unlockRequests = payload.unlockRequests.filter(
           (unlockReq) => unlockReq.id !== action.meta.arg,
         );
+      }
+    });
+
+    builder.addCase(deleteActivityItems.succeeded, (state, { meta: { arg } }) => {
+      const payload = Req.payload(state.request);
+      if (!payload) return;
+      const user = payload.userActivity.find((user) => user.id === arg.userId);
+      if (user) {
+        user.numUnreviewed -= arg.itemRootIds.length;
       }
     });
   },
