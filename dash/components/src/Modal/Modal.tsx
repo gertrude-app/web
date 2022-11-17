eebb9aa04d32879bcf2aa8c84632cdc5e9b5a8fd
Jared Henderson <jaredh159@users.noreply.github.com>
2022-11-17 10:54:31 -0500

    dash: fix modal dismiss logic causing key creator to save on dismiss

--- dash/components/src/Modal/Modal.tsx
+++ dash/components/src/Modal/Modal.tsx
@@ -77,7 +77,12 @@ const Modal: React.FC<Props> = ({
         as="div"
         className="relative z-30"
         onClose={
-          onDismiss ?? (secondary?.action ?? type === `error` ? primary.action : () => {})
+          onDismiss ??
+          (secondary?.action
+            ? secondary.action
+            : type === `error`
+            ? primary.action
+            : () => {})
         }
       >
         <Transition.Child
