f56543514d09b0f20e76af03c57212d3ed413138
Jared Henderson <jaredh159@users.noreply.github.com>
2022-12-12 14:27:36 -0500

    SAVEPOINT

--- site/components/src/OgImage.tsx
+++ site/components/src/OgImage.tsx
@@ -2,25 +2,25 @@ import { Logo } from '@shared/components';
 import React from 'react';
 
 const OgImage: React.FC = () => (
-  <section className="bg-gradient-to-b from-violet-500 to-fuchsia-500 flex justify-center items-stretch flex-col w-[1200px] h-[627px]">
-    <div className="h-1/2 flex items-center justify-center">
+  <section className="bg-gradient-to-b bg-gradient-to-b from-gray-900 to-gray-800 flex justify-center items-stretch flex-col w-[1280px] h-[720px]">
+    <div className="h-1/2 flex items-start mt-12 justify-center">
       <Logo
         type="inverted"
-        size={150}
-        textSize="text-[155px]"
-        innerColorOverride="rgb(153 74 239)"
+        size={80}
+        textSize="text-[80px]"
+        innerColorOverride="rgb(26 33 44)"
       />
     </div>
     <div className="h-1/2 grow text-[40px] text-center flex flex-col space-y-5">
-      <div className="text-white text-opacity-70 font-bold">
-        Serious internet safety for{` `}
+      <div className="text-white text-opacity-70 font-bold opacity-0">
+        Serious parental controls for{` `}
         <b className="text-opacity-90 text-white font-extrabold">Apple computers.</b>
       </div>
-      <div className="text-white text-opacity-70 font-bold">
+      <div className="text-white text-opacity-70 font-bold opacity-0">
         <b className="text-opacity-90 text-white font-extrabold">Peace of mind</b>
         {` `}for parents.
       </div>
-      <div className="text-white text-opacity-70 font-bold text-[32px] pt-10 antialiased">
+      <div className="*text-white text-fuchsia-400 text-opacity-70 font-bold text-[32px] pt-10 antialiased">
         https://gertrude.app
       </div>
     </div>
