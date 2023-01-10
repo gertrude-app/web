8d73b2f0872e8b6bf2f53b5d1c2132e75cef4027
Kiah <miciahjohnhenderson@gmail.com>
2023-01-10 12:16:57 -0500

    site: curved hero and docs logo

--- site/components/src/HeroBlock.tsx
+++ site/components/src/HeroBlock.tsx
@@ -3,17 +3,23 @@ import cx from 'classnames';
 
 const HeroBlock: React.FC = () => {
   return (
-    <section className="flex justify-center items-center sm:p-20 p-10 py-20 bg-gradient-to-b from-violet-500 to-fuchsia-500">
-      <h1
-        className={cx(
-          `lg:text-6xl text-5xl font-extrabold text-center leading-[120%] text-white text-opacity-60`,
-        )}
-      >
-        Finally, real <span className="text-white text-opacity-90">internet safety</span>
-        {` `}
-        is possible
-      </h1>
-    </section>
+    <>
+      <section className="flex justify-center items-center sm:p-20 p-10 pt-20 bg-gradient-to-b from-violet-500 to-fuchsia-500 relative z-10">
+        <h1
+          className={cx(
+            `lg:text-6xl text-5xl font-extrabold text-center leading-[120%] text-white text-opacity-60`,
+          )}
+        >
+          Finally, real{' '}
+          <span className="text-white text-opacity-90">internet safety</span>
+          {` `}
+          is possible
+        </h1>
+      </section>
+      <div className="h-16 bg-[#f7f8fA] flex justify-center items-center relative">
+        <div className="bg-fuchsia-500 h-full w-[120vw] rounded-[50%] absolute -top-8" />
+      </div>
+    </>
   );
 };
 
