99c593a5010a7f60e495dbcf56175a05477ad524
Jared Henderson <jaredh159@users.noreply.github.com>
2023-02-21 14:15:54 -0500

    appviews: refined store, testing integration with macapp

--- appviews/src/MenuBar/menubar-store.ts
+++ appviews/src/MenuBar/menubar-store.ts
@@ -1,38 +1,5 @@
-import type { Action, Store } from '../lib/store';
-import { BaseStore } from '../lib/store';
-
-type State = AppState & { loading: boolean };
-
-export class MenuBarStore
-  extends BaseStore<State, AppEvent>
-  implements Store<State, Action<AppState>, AppEvent, Props>
-{
-  initializer(): State {
-    return { state: `notConnected`, loading: true };
-  }
-
-  reducer(state: State, _action: Action<AppState>): State {
-    return state;
-  }
-
-  selector(
-    _state: AppState,
-    _dispatch: (action: Action<AppState>) => unknown,
-    _emit: (event: AppEvent) => unknown,
-  ): Props {
-    return {
-      state: `notConnected`,
-      onResumeFilterClicked: () => {},
-      onSuspendFilterClicked: () => {},
-      onRefreshRulesClicked: () => {},
-      onAdministrateClicked: () => {},
-      onViewNetworkTrafficClicked: () => {},
-    };
-  }
-}
-
-const store = new MenuBarStore();
-export default store;
+import type { Action } from '../lib/store';
+import { Store } from '../lib/store';
 
 type FilterState =
   | { state: 'off' }
@@ -53,7 +20,8 @@ type AppEvent =
   | 'suspendFilterClicked'
   | 'refreshRulesClicked'
   | 'administrateClicked'
-  | 'viewNetworkTrafficClicked';
+  | 'viewNetworkTrafficClicked'
+  | 'fakeConnect';
 
 export type Props = AppState & {
   onResumeFilterClicked(): unknown;
@@ -61,4 +29,32 @@ export type Props = AppState & {
   onRefreshRulesClicked(): unknown;
   onAdministrateClicked(): unknown;
   onViewNetworkTrafficClicked(): unknown;
+  onConnectToUserClicked(): unknown;
 };
+
+export class MenuBarStore extends Store<AppState, AppEvent, Props> {
+  initializer(): AppState {
+    return { state: `notConnected` };
+  }
+
+  reducer(state: AppState, action: Action<AppState>): AppState {
+    switch (action.type) {
+      case `receivedUpdatedAppState`:
+        return action.appState;
+    }
+  }
+
+  selector(state: AppState, emit: (event: AppEvent) => unknown): Props {
+    return {
+      ...state,
+      onResumeFilterClicked: () => emit(`resumeFilterClicked`),
+      onSuspendFilterClicked: () => emit(`suspendFilterClicked`),
+      onRefreshRulesClicked: () => emit(`refreshRulesClicked`),
+      onAdministrateClicked: () => emit(`administrateClicked`),
+      onViewNetworkTrafficClicked: () => emit(`viewNetworkTrafficClicked`),
+      onConnectToUserClicked: () => emit(`fakeConnect`),
+    };
+  }
+}
+
+export default new MenuBarStore();
--- appviews/src/lib/store.ts
+++ appviews/src/lib/store.ts
@@ -1,23 +1,19 @@
 import { useReducer, useEffect } from 'react';
 import type React from 'react';
 
-export type Store<State, Action, AppEvent, Props, InitArg = void> = {
-  initializer: (arg: InitArg) => State;
-  bind: (window: any, dispatch: React.Dispatch<Action>) => void;
-  reducer: (state: State, action: Action) => State;
-  emitter: (window: any) => (event: AppEvent) => unknown;
-  selector: (
-    state: State,
-    dispatch: (action: Action) => unknown,
-    emit: (event: AppEvent) => unknown,
-  ) => Props;
-};
+export type Action<AppState, ViewAction = never> =
+  | { type: 'receivedUpdatedAppState'; appState: AppState }
+  | ViewAction;
 
-export type Action<AppState, ViewAction = Record<never, never>> =
-  | { type: 'receivedUpdatedAppState'; appState: AppState } & ViewAction;
-
-export class BaseStore<AppState, AppEvent> {
-  bind(window: any, dispatch: React.Dispatch<Action<AppState>>): void {
+export abstract class Store<
+  AppState,
+  AppEvent,
+  Props,
+  InitArg = void,
+  ViewAction = never,
+  ViewState = Record<never, never>,
+> {
+  bind(window: any, dispatch: React.Dispatch<Action<AppState, ViewAction>>): void {
     window.updateAppState = (appState: AppState) => {
       dispatch({ type: `receivedUpdatedAppState`, appState });
     };
@@ -25,15 +21,26 @@ export class BaseStore<AppState, AppEvent> {
 
   emitter(window: any): (event: AppEvent) => unknown {
     return (event: AppEvent) => {
-      window.webkit.messageHandlers.appView.postMessage({
-        message: JSON.stringify({ event }),
-      });
+      window.webkit.messageHandlers.appView.postMessage(event);
     };
   }
+
+  abstract initializer(arg: InitArg): AppState & ViewState;
+
+  abstract reducer(
+    state: AppState & ViewState,
+    action: Action<AppState, ViewAction>,
+  ): AppState & ViewState;
+
+  abstract selector(
+    state: AppState & ViewState,
+    emit: (event: AppEvent) => unknown,
+    dispatch: (action: ViewAction) => unknown,
+  ): Props;
 }
 
-export function containerize<AppState, Action, AppEvent, Props>(
-  store: Store<AppState, Action, AppEvent, Props>,
+export function containerize<AppState, AppEvent, Props>(
+  store: Store<AppState, AppEvent, Props>,
   component: React.FC<Props>,
 ): React.FC {
   return () => {
@@ -43,7 +50,7 @@ export function containerize<AppState, Action, AppEvent, Props>(
       store.bind(window, dispatch);
     }, []);
 
-    const props = store.selector(state, dispatch, store.emitter(window));
+    const props = store.selector(state, store.emitter(window), dispatch);
 
     return component(props);
   };
