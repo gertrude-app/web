77914204d4caf8a1701d1990ad9171f242b20a9a
Kiah <miciahjohnhenderson@gmail.com>
2023-04-06 12:10:11 -0400

    appviews: made healthcheck screen

--- appviews/src/Administrate/Administrate.tsx
+++ appviews/src/Administrate/Administrate.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import cx from 'classnames';
-import { Button } from '@shared/components';
-import FilterStatusBlock from './FilterStatusBlock';
-import HealthCheckPreviewBlock from './HealthCheckPreviewBlock';
-import SidebarNav from './SidebarNav';
-import AppVersionBlock from './AppVersionBlock';
-import UserBlock from './UserBlock';
+import SidebarNav from './subcomponents/SidebarNav';
+import HomeScreen from './screens/HomeScreen';
+import HealthCheckScreen from './screens/HealthCheckScreen';
+import ExemptUsersScreen from './screens/ExemptUsersScreen';
 
 export type Page = 'home' | 'health_check' | 'exempt_users';
+export type HealthCheckStatus = {
+  appVersion: string;
+  mostRecentAppVersion: string;
+  filterVersion: string;
+  mostRecentFilterVersion: string;
+  screenRecordingPermission: boolean;
+  keystrokeRecordingPermission: boolean;
+  isAdministrator: boolean;
+  filterToAppCommunicationVerified: boolean;
+  notificationSetting: 'banners' | 'alert' | 'none';
+  accountStatus: 'active' | 'inactive';
+  keysLoaded: number;
+};
 
 interface Props {
-  healthCheck: {
-    appVersion: string;
-    mostRecentAppVersion: string;
-    filterVersion: string;
-    mostRecentFilterVersion: string;
-    screenRecordingPermission: boolean;
-    keystrokeRecordingPermission: boolean;
-    isAdministrator: boolean;
-    filterToAppCommunicationVerified: boolean;
-    notificationSetting: 'banners' | 'alert' | 'none';
-    accountStatus: 'active' | 'inactive';
-    keysLoaded: number;
-  };
+  healthCheck: HealthCheckStatus;
   filterStatus: 'on' | 'off' | 'suspended';
   userName: string;
   screenshotMonitoringEnabled: boolean;
@@ -37,8 +35,6 @@ const Administrate: React.FC<Props> = ({
   keystrokeMonitoringEnabled,
 }) => {
   const [page, setPage] = React.useState<Page>(`home`);
-  const [debuggerOpen, setDebuggerOpen] = React.useState(false);
-  const [debugging, setDebugging] = React.useState(false);
 
   const failingChecksCount = [
     healthCheck.appVersion === healthCheck.mostRecentAppVersion,
@@ -51,63 +47,52 @@ const Administrate: React.FC<Props> = ({
     healthCheck.keysLoaded > 0,
   ].filter((item) => !item).length;
 
+  let pageElement = (
+    <HomeScreen
+      page={page}
+      setPage={setPage}
+      filterStatus={filterStatus}
+      failingChecksCount={failingChecksCount}
+      appVersion={healthCheck.appVersion}
+      userName={userName}
+      keystrokeMonitoringEnabled={keystrokeMonitoringEnabled}
+      screenshotMonitoringEnabled={screenshotMonitoringEnabled}
+    />
+  );
+
+  switch (page) {
+    case `home`:
+      pageElement = (
+        <HomeScreen
+          page={page}
+          setPage={setPage}
+          filterStatus={filterStatus}
+          failingChecksCount={failingChecksCount}
+          appVersion={healthCheck.appVersion}
+          userName={userName}
+          keystrokeMonitoringEnabled={keystrokeMonitoringEnabled}
+          screenshotMonitoringEnabled={screenshotMonitoringEnabled}
+        />
+      );
+      break;
+    case `health_check`:
+      pageElement = (
+        <HealthCheckScreen
+          healthCheck={healthCheck}
+          failingChecksCount={failingChecksCount}
+        />
+      );
+      break;
+    case `exempt_users`:
+      pageElement = <ExemptUsersScreen />;
+      break;
+  }
+
   return (
     <div className="flex h-full">
       <SidebarNav page={page} setPage={setPage} />
-      <main className="flex-grow bg-white dark:bg-slate-900 rounded-br-xl flex flex-col justify-between p-4">
-        <div className="flex flex-col space-y-4">
-          <div className="flex space-x-4">
-            <FilterStatusBlock filterStatus={filterStatus} />
-            <HealthCheckPreviewBlock
-              failingChecksCount={failingChecksCount}
-              setPage={setPage}
-            />
-          </div>
-          <AppVersionBlock appVersion={healthCheck.appVersion} />
-          <UserBlock
-            keystrokeMonitoringEnabled={keystrokeMonitoringEnabled}
-            screenshotMonitoringEnabled={screenshotMonitoringEnabled}
-            userName={userName}
-          />
-        </div>
-        <footer className="mt-4 flex justify-between items-end">
-          <div
-            className={cx(
-              `rounded-xl border-slate-200 dark:border-slate-800 border flex items-center justify-start overflow-hidden [transition:200ms]`,
-              debuggerOpen ? `w-[200px]` : `w-[42px]`,
-            )}
-          >
-            <button
-              className="rounded-xl text-xl w-10 h-10 text-slate-400 dark:text-slate-600 hover:bg-slate-50 dark:hover:bg-slate-800 hover:text-slate-500 transition duration-100 shrink-0 relative flex justify-center items-center"
-              onClick={() => setDebuggerOpen(!debuggerOpen)}
-            >
-              <i
-                className={cx(
-                  `fa-solid fa-circle text-xs transition duration-100 absolute animate-ping text-red-500`,
-                  debugging ? `block` : `hidden`,
-                )}
-              />
-              <i
-                className={cx(
-                  `fa-solid transition duration-100 relative`,
-                  debugging ? `fa-circle text-[10px] text-red-500` : `fa-bug-slash`,
-                )}
-              />
-            </button>
-            <button
-              className={cx(
-                `mx-1.5 bg-slate-50 dark:bg-slate-900 text-slate-500 dark:text-slate-400 font-medium border-[0.5px] border-slate-200 dark:border-slate-600 hover:border-slate-300 hover:bg-white hover:text-slate-600 dark:hover:bg-slate-800 transition duration-200 rounded-lg px-2 py-0.5 active:scale-95 whitespace-nowrap`,
-                debuggerOpen ? `opacity-100` : `opacity-0`,
-              )}
-              onClick={() => setDebugging(!debugging)}
-            >
-              {debugging ? `Stop debugging` : `Send debug data`}
-            </button>
-          </div>
-          <Button type="button" onClick={() => {}} color="warning">
-            Quit app
-          </Button>
-        </footer>
+      <main className="flex-grow bg-white dark:bg-slate-900 rounded-br-xl">
+        {pageElement}
       </main>
     </div>
   );
---
+++ appviews/src/Administrate/screens/ExemptUsersScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const ExemptUsersScreen: React.FC<Props> = ({}) => {
+  return <h1>ExemptUsersScreen</h1>;
+};
+
+export default ExemptUsersScreen;
