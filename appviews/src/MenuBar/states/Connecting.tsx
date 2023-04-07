08c30756b1127db56596beff8aa639600062d9d5
Kiah <miciahjohnhenderson@gmail.com>
2023-04-07 14:49:31 -0400

    appviews: made states for connecting and connection failed for menu bar

--- appviews/src/MenuBar/states/Connecting.tsx
+++ appviews/src/MenuBar/states/Connecting.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { MenuBarSized } from '../MenuBar';
 
 const Connecting: React.FC = () => {
-  return <MenuBarSized>Connecting...</MenuBarSized>;
+  return (
+    <MenuBarSized className="flex flex-col justify-center items-center">
+      <div className="bg-gradient-to-br from-indigo-500 to-fuchsia-500 w-16 h-16 rounded-full flex justify-center items-center">
+        <i className="fas fa-spinner text-white text-3xl animate-spin" />
+      </div>
+    </MenuBarSized>
+  );
 };
 
 export default Connecting;
