fe547f821fcc3c552b10d8ad0d5e9a7d4b77d12d
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-04-27 23:13:30 -0400

    dash: cleaned up sidebar and dash dash

--- dash/components/src/Chrome/SidebarOption.tsx
+++ dash/components/src/Chrome/SidebarOption.tsx
@@ -24,27 +24,25 @@ const SidebarOption: React.FC<Props> = ({
     onClick={onClick}
     className={cx(
       `ScrollTop flex justify-start items-center`,
-      `cursor-pointer transition duration-75 select-none`,
+      `cursor-pointer transition duration-75 select-none rounded-xl`,
       `bg-violet-500 bg-opacity-0`,
       `focus:outline-none focus:ring-2 focus:ring-inset focus:ring-violet-500/50`,
       isSelected ? `bg-opacity-20` : `hover:bg-opacity-10`,
       collapsed
-        ? `py-[1.4em] pl-[1.65em]`
-        : `rounded-xl py-[6px] sm:py-[8px] md:py-[12px] px-5 my-3 `,
+        ? `py-[1.2em] my-[0.3em] pl-[1.1em] flex justify-center items-center`
+        : `py-[6px] sm:py-[8px] md:py-[12px] px-5 my-3 `,
     )}
   >
     <i
       aria-hidden="true"
       className={cx(
         `fa fa-${icon} mr-2.5 sm:mr-4 text-xl sm:text-2xl leading-none w-7`,
-        isSelected
-          ? `bg-gradient-to-br from-indigo-500 to-fuchsia-500 text-transparent bg-clip-text [-webkit-background-clip:text;]`
-          : `text-violet-300 text-opacity-40`,
+        isSelected ? `text-violet-400` : `text-violet-300 text-opacity-40`,
       )}
     />
     <h2
       className={cx(
-        `text-white text-[18px] sm:text-xl antialiased text-opacity-60 font-bold`,
+        `text-slate-300/80 text-[18px] sm:text-xl antialiased font-bold`,
         collapsed ? `hidden` : `block`,
       )}
     >
