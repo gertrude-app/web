2ac49cdeeb0673a65cda0ab7a08a416a9a162bc7
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-05-30 11:05:18 -0400

    storybook: updated stories for keylist

--- storybook/src/dash/Keychains/KeyListKey.stories.tsx
+++ storybook/src/dash/Keychains/KeyListKey.stories.tsx
@@ -11,25 +11,72 @@ type Story = StoryObj<typeof meta>;
 
 export const StandardWebsite: Story = props({
   record: keyExamples.standardWeb,
+  type: `list`,
 });
 
 export const StrictWebsite: Story = props({
   record: keyExamples.strictWeb,
+  type: `list`,
 });
 
 export const NormalApp: Story = props({
   record: keyExamples.skeletonApp,
+  type: `list`,
 });
 
 export const NormalAppWithBundleId: Story = props({
   record: keyExamples.skeletonAppBundleId,
+  type: `list`,
 });
 
 export const SingleAppKey: Story = props({
   record: keyExamples.singleApp,
+  type: `list`,
 });
 
 export const UnrestrictedForAllApps: Story = props({
+  type: `list`,
+  record: {
+    id: `6`,
+    keychainId: `1`,
+    key: {
+      type: `domain`,
+      domain: `rad-vim-themes.biz`,
+      scope: {
+        type: `unrestricted`,
+      },
+    },
+    comment: undefined,
+  },
+});
+
+export const StandardWebsiteTable: Story = props({
+  record: keyExamples.standardWeb,
+  type: `table`,
+});
+
+export const StrictWebsiteTable: Story = props({
+  record: keyExamples.strictWeb,
+  type: `table`,
+});
+
+export const NormalAppTable: Story = props({
+  record: keyExamples.skeletonApp,
+  type: `table`,
+});
+
+export const NormalAppWithBundleIdTable: Story = props({
+  record: keyExamples.skeletonAppBundleId,
+  type: `table`,
+});
+
+export const SingleAppKeyTable: Story = props({
+  record: keyExamples.singleApp,
+  type: `table`,
+});
+
+export const UnrestrictedForAllAppsTable: Story = props({
+  type: `table`,
   record: {
     id: `6`,
     keychainId: `1`,
