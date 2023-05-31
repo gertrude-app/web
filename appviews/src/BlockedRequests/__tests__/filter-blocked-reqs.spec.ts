e03fe7bf3d537d5e901a430f08270eeb3d492a0f
Jared Henderson <jaredh159@users.noreply.github.com>
2023-05-31 09:13:57 -0400

    appviews: extract, test, fix blocked requests filtering

---
+++ appviews/src/BlockedRequests/__tests__/filter-blocked-reqs.spec.ts
@@ -0,0 +1,40 @@
+import { expect, it, describe } from 'vitest';
+import { filterVisibleRequests } from '../BlockedRequests';
+
+describe(`filterVisibleRequests())`, () => {
+  it(`removes tcp requests when tcpOnly true`, () => {
+    const requests = [
+      { protocol: `tcp` as const, searchableText: `tcp 1`, time: `1` },
+      { protocol: `udp` as const, searchableText: `udp 1`, time: `2` },
+      { protocol: `tcp` as const, searchableText: `tcp 2`, time: `3` },
+    ];
+
+    const all = filterVisibleRequests(requests, ``, false);
+    expect(all).toEqual([...requests].reverse());
+
+    const tcpOnly = filterVisibleRequests(requests, ``, true);
+    expect(tcpOnly).toEqual([
+      { protocol: `tcp` as const, searchableText: `tcp 2`, time: `3` },
+      { protocol: `tcp` as const, searchableText: `tcp 1`, time: `1` },
+    ]);
+  });
+
+  it(`filters by search text`, () => {
+    const requests = [
+      { protocol: `tcp` as const, searchableText: `jim jam bar`, time: `3` },
+      { protocol: `udp` as const, searchableText: `Foo quux bar`, time: `2` },
+      { protocol: `tcp` as const, searchableText: `foo bar baz foobar`, time: `1` },
+    ];
+
+    let filtered = filterVisibleRequests(requests, `bar jim`, false);
+    expect(filtered).toEqual([
+      { protocol: `tcp` as const, searchableText: `jim jam bar`, time: `3` },
+    ]);
+
+    filtered = filterVisibleRequests(requests, `foo BAR`, false);
+    expect(filtered).toEqual([
+      { protocol: `udp` as const, searchableText: `Foo quux bar`, time: `2` },
+      { protocol: `tcp` as const, searchableText: `foo bar baz foobar`, time: `1` },
+    ]);
+  });
+});
