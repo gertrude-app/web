1ec5de043a839b2d88ebb8af0f38d796851654b2
Jared Henderson <jaredh159@users.noreply.github.com>
2023-06-14 09:28:28 -0400

    wip: fix cypress login fail

--- dash/app/src/components/routes/CheckoutSuccess.tsx
+++ dash/app/src/components/routes/CheckoutSuccess.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { useSearchParams, Navigate } from 'react-router-dom';
 import { FullscreenModalForm } from '@dash/components';
 import { Result } from '@dash/types';
-import { useFireAndForget } from '../../hooks';
+import { useAuth, useFireAndForget } from '../../hooks';
 import Current from '../../environment';
 
 const CheckoutSuccess: React.FC = () => {
+  const { login } = useAuth();
   const [params] = useSearchParams();
   const sessionId = params.get(`session_id`);
 
-  const query = useFireAndForget(() => {
-    if (!sessionId) return Result.resolveUnexpected(`4c61dc17`);
-    return Current.api.handleCheckoutSuccess({ stripeCheckoutSessionId: sessionId });
-  });
+  const query = useFireAndForget(
+    () => {
+      if (!sessionId) return Result.resolveUnexpected(`4c61dc17`);
+      return Current.api.handleCheckoutSuccess({ stripeCheckoutSessionId: sessionId });
+    },
+    { onReceive: ({ adminId, token }) => login(adminId, token) },
+  );
 
   if (query.isLoading) {
     return <FullscreenModalForm request="ongoing" text="Completing signup..." />;
