97cace0f9ef52bfd3ca39615fb501655a719be15
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-09-21 13:29:46 -0400

    appviews: made allow screenshots screens

--- appviews/src/Onboarding/Steps/ConnectChild.tsx
+++ appviews/src/Onboarding/Steps/ConnectChild.tsx
@@ -1,5 +1,6 @@
-import { Loading } from '@shared/components';
 import React from 'react';
+import cx from 'classnames';
+import { Button, Loading } from '@shared/components';
 import type { AppEvent, RequestState, ViewAction } from '../onboarding-store';
 
 interface Props {
@@ -10,60 +11,111 @@ interface Props {
 }
 
 const ConnectChild: React.FC<Props> = ({ emit, dispatch, connectionCode, request }) => {
-  switch (request.state) {
-    case `ongoing`:
-      return <Loading />;
-    case `idle`:
-      return (
-        <div>
-          <h1 className="text-3xl">Connect a child</h1>
-          <p>Enter the 6-digit connection code from the Gertrude parent's site</p>
-          <input type="text" onChange={() => {}} value={connectionCode} />
+  return (
+    <div className="h-full flex flex-col justify-center items-center relative">
+      <div
+        className={cx(
+          `absolute transition-[opacity,transform] duration-500 flex flex-col items-center`,
+          request.state === `idle` || request.state === `ongoing`
+            ? `opacity-100`
+            : `opacity-0 translate-y-12 pointer-events-none`,
+        )}
+      >
+        <h1 className="text-3xl font-bold">Connect a child</h1>
+        <p className="text-lg text-slate-500 mt-4">
+          Enter the 6-digit connection code from the Gertrude parent's site:
+        </p>
+        <div className="flex mt-8 gap-4">
+          <input
+            type="text"
+            onChange={() => alert(`TODO`)}
+            value={connectionCode}
+            className="shadow-lg shadow-slate-300/50 rounded-2xl text-3xl px-6 py-4 text-slate-600 font-bold font-mono w-64 tracking-[8px] border-2 border-white focus:border-violet-400 focus:ring-0 transition-[border-color,transform,box-shadow] duration-300 focus:shadow-xl focus:shadow-slate-300/50 focus:-translate-y-0.5 placeholder:text-slate-200"
+            placeholder="******"
+          />
           <button
-            className="bg-blue-500 text-white font-bold py-2 px-4"
             onClick={() =>
               dispatch({ type: `updateConnectionCode`, code: connectionCode })
             }
+            className="bg-gradient-to-r from-violet-500 to-fuchsia-500 px-6 text-xl flex justify-center items-center gap-3 font-medium text-white rounded-2xl shadow-xl shadow-violet-500/40 cursor-pointer hover:scale-[102%] active:scale-[98%] hover:shadow-2xl hover:shadow-violet-500/50 active:shadow-lg active:shadow-violet-500/30 transition-[transform,box-shadow] duration-300 outline-none focus:ring-0 w-44"
           >
-            Submit &rarr;
+            <div
+              className={cx(
+                'flex gap-3 items-center transition-[opacity,transform] duration-300 absolute',
+                request.state === 'idle' ? `opacity-100` : `opacity-0 scale-50`,
+              )}
+            >
+              <span>Connect</span>
+              <i className="fa-solid fa-arrow-right" />
+            </div>
+            <div
+              className={cx(
+                'flex gap-3 items-center transition-[opacity,transform] duration-300 absolute',
+                request.state === 'ongoing' ? `opacity-100` : `opacity-0 translate-y-4`,
+              )}
+            >
+              <i className="fa-solid fa-spinner animate-spin" />
+            </div>
           </button>
         </div>
-      );
-    case `failed`:
-      return (
-        <div>
-          <h1 className="text-3xl">Uh-oh, something went wrong</h1>
-          <p>Failed to connect child</p>
-          <button
-            className="bg-blue-500 text-white font-bold py-2 px-4"
+      </div>
+      <div
+        className={cx(
+          `absolute transition-[opacity,transform] duration-500 flex justify-center items-center flex-col bg-white p-12 rounded-3xl shadow-lg shadow-slate-300/30`,
+          request.state === `succeeded`
+            ? `opacity-100`
+            : `opacity-0 translate-y-12 pointer-events-none`,
+        )}
+      >
+        <h1 className="text-3xl font-bold">Success!</h1>
+        <p className="mt-2 mb-6 text-lg text-slate-500">
+          You've connected this computer to the child{' '}
+          <span className="font-medium text-slate-600">
+            {request.state === `succeeded` && request.payload}
+          </span>
+          .
+        </p>
+        <Button
+          type="button"
+          onClick={() => emit({ case: `primaryBtnClicked` })}
+          color="primary"
+          size="large"
+        >
+          Next
+          <i className="fa-solid fa-arrow-right ml-2" />
+        </Button>
+      </div>
+      <div
+        className={cx(
+          `absolute transition-[opacity,transform] duration-500 flex flex-col items-center border-2 p-16 rounded-3xl border-slate-200/50 border-dashed`,
+          request.state === `failed`
+            ? `opacity-100`
+            : `opacity-0 translate-y-12 pointer-events-none`,
+        )}
+      >
+        <h1 className="text-3xl font-bold">Uh-oh, something went wrong</h1>
+        <p className="mt-2 mb-6 text-lg text-slate-500">Failed to connect child</p>
+        <div className="flex flex-col justify-center w-80 gap-4">
+          <Button
+            type="button"
             onClick={() => emit({ case: `primaryBtnClicked` })}
+            color="primary"
+            size="large"
           >
             Try again
-          </button>
-          <button
-            className="bg-gray-400 text-white font-bold py-2 px-4"
+          </Button>
+          <Button
+            type="button"
             onClick={() => emit({ case: `secondaryBtnClicked` })}
+            color="secondary"
+            size="large"
           >
             Get help
-          </button>
+          </Button>
         </div>
-      );
-    case `succeeded`:
-      return (
-        <div>
-          <h1 className="text-3xl">Success!</h1>
-          <p>
-            You've connected this computer to the child <code>{request.payload}</code>
-          </p>
-          <button
-            className="bg-blue-500 text-white font-bold py-2 px-4"
-            onClick={() => emit({ case: `primaryBtnClicked` })}
-          >
-            Next &rarr;
-          </button>
-        </div>
-      );
-  }
+      </div>
+    </div>
+  );
 };
 
 export default ConnectChild;
