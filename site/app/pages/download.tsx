3be3e1f06a2c85cca8a3636adba7cb73d1b0a3b0
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-31 16:03:08 -0400

    site: redesigned download page to increase clarity

--- site/app/pages/download.tsx
+++ site/app/pages/download.tsx
@@ -1,4 +1,4 @@
-import { Logo } from '@shared/components';
+import { AppIcon } from '@shared/components';
 import { Chrome } from '@site/components';
 import type { NextPage } from 'next';
 import Catalina from '../public/macos-catalina.png';
@@ -9,32 +9,39 @@ import Sonoma from '../public/macos-sonoma.png';
 
 const Download: NextPage = () => (
   <Chrome>
-    <section className="bg-gradient-to-b from-violet-500 to-fuchsia-500 flex-grow flex flex-col justify-center items-center p-6 py-10">
-      <div className="bg-slate-50 shadow-xl rounded-3xl py-8 px-6 xs:px-10 sm:px-12 flex flex-col items-center mb-6">
-        <Logo type="default" iconOnly size={90} className="mb-4 xs:mb-6" />
-        <h1 className="text-3xl font-inter mb-4 xs:mb-4 text-center">
-          Download Gertrude
-        </h1>
-        <p className="text-slate-500 mb-6 text-center">
-          We currently support the following operating systems:
-        </p>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 self-stretch">
-          <OperatingSystem name="Catalina" img={Catalina.src} />
-          <OperatingSystem name="Big Sur" img={BigSur.src} />
-          <OperatingSystem name="Monterey" img={Monterey.src} />
-          <OperatingSystem name="Ventura" img={Ventura.src} />
-          <OperatingSystem name="Sonoma" img={Sonoma.src} />
+    <section className="bg-gradient-to-b from-violet-500 to-fuchsia-500 flex-grow flex flex-col justify-center items-center py-0 xs:py-8 sm:py-16 px-0 xs:px-6 sm:px-8 md:px-12 lg:px-16">
+      <div className="bg-white/90 xs:rounded-3xl flex gap-12">
+        <div className="lg+:shrink-0 lg+:max-w-2xl py-16 xs:py-8 sm:py-12 md:py-16 lg:py-20 px-6 xs:px-8 sm:px-12 md:px-16 lg:px-20 flex flex-col items-center sm:items-start">
+          <h1 className="text-4xl md:text-5xl font-bold text-slate-900 text-center sm:text-left">
+            Download Gertrude
+          </h1>
+          <p className="text-lg md:text-xl text-slate-500 mt-4 max-w-3xl text-center sm:text-left">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum
+            sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore
+            adipisicing minim sint cillum sint consectetur cupidatat.
+          </p>
+          <a
+            href="https://gertrude.nyc3.digitaloceanspaces.com/releases/Gertrude.dmg"
+            className="mt-8 text-2xl font-semibold text-white bg-gradient-to-r from-violet-500 to-fuchsia-500 px-6 xs:px-8 py-3 xs:py-4 rounded-3xl flex items-center gap-4 hover:shadow-lg hover:scale-[102%] transition-[transform,box-shadow] hover:shadow-fuchsia-500/20 duration-300 active:scale-[98%] active:shadow-none w-fit"
+          >
+            <i className="fa-solid fa-download" />
+            <span>Download for macOS</span>
+          </a>
+          <div className="mt-16 mb-4 text-slate-400 text-lg md:text-xl bg-[#f6effd] relative w-fit sm:pl-2 sm:pr-3 sm:-ml-1 text-center">
+            We currently support these operating systems:
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 sm:border border-violet-300/80 sm:p-4 sm:pt-6 md:pt-8 sm:-m-4 sm:-mt-7 rounded-3xl border-dashed self-stretch">
+            <OperatingSystem name={`Catalina`} img={Catalina.src} />
+            <OperatingSystem name={`Big Sur`} img={BigSur.src} />
+            <OperatingSystem name={`Monterey`} img={Monterey.src} />
+            <OperatingSystem name={`Ventura`} img={Ventura.src} />
+            <OperatingSystem name={`Sonoma`} img={Sonoma.src} />
+          </div>
+        </div>
+        <div className="flex-grow lg+:flex justify-center items-center p-12 xl:p-20 bg-violet-500/10 rounded-r-3xl hidden">
+          <AppIcon />
         </div>
       </div>
-      <a
-        href="https://gertrude.nyc3.digitaloceanspaces.com/releases/Gertrude.dmg"
-        className="bg-gradient-to-b from-white via-white to-fuchsia-100 px-8 py-4 text-2xl rounded-xl shadow-xl shadow-fuchsia-600 font-medium hover:-translate-y-1 transition-[box-shadow,transform] duration-100 hover:shadow-2xl active:translate-y-1 active:shadow-md active:scale-95 hover:shadow-fuchsia-700 flex justify-center items-center"
-      >
-        <i className="fa-solid fa-download mr-4 bg-gradient-to-br from-indigo-600 to-fuchsia-600 bg-clip-text text-transparent [-webkit-background-clip:text;]" />
-        <span className="bg-gradient-to-br from-indigo-600 to-fuchsia-600 bg-clip-text text-transparent [-webkit-background-clip:text;]">
-          Download
-        </span>
-      </a>
     </section>
   </Chrome>
 );
@@ -47,9 +54,9 @@ interface OperatingSystemProps {
 }
 
 const OperatingSystem: React.FC<OperatingSystemProps> = ({ name, img }) => (
-  <div className="flex flex-row md:flex-col items-center p-3 rounded-xl shadow-lg bg-white flex-grow shrink-0">
+  <div className="flex flex-col items-center p-3 rounded-2xl bg-violet-200/50 flex-grow shrink-0">
     <img src={img} width={70} height={70} alt="macOS operating system icon" />
-    <div className="mt-1 flex flex-col items-start md:items-center ml-2 md:ml-0">
+    <div className="mt-1 flex flex-col items-center ml-0">
       <label className="text-sm text-slate-500 -mb-1">macOS</label>
       <h3 className="font-bold text-slate-700 text-lg">{name}</h3>
     </div>
