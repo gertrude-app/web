e5a131f7d01769b5eb9f00b9fa1838a0d4cd8be4
Jared Henderson <jaredh159@users.noreply.github.com>
2023-12-20 13:56:01 -0500

    dash: merge stray keystroke lines into suspension

--- dash/components/src/Users/Activity/UserActivityFeed.tsx
+++ dash/components/src/Users/Activity/UserActivityFeed.tsx
@@ -3,10 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '@shared/components';
 import { UndoMainPadding } from '../../Chrome/Chrome';
 import EmptyState from '../../EmptyState';
-import KeystrokesViewer from './KeystrokesViewer';
-import ScreenshotViewer from './ScreenshotViewer';
 import FeedHeader from './FeedHeader';
 import ReviewDayWrapper from './ReviewDayWrapper';
+import DeletableActivityChunks from './DeletableActivityChunks';
 
 interface Screenshot {
   type: 'Screenshot';
@@ -51,7 +50,11 @@ const UserActivityFeed: React.FC<Props> = ({
       <FeedHeader date={date} numItems={items.length} numDeleted={numDeleted} />
       {items.length > 0 ? (
         <ReviewDayWrapper>
-          {deleteableChunks(items, chunkSize, deleteItems)}
+          <DeletableActivityChunks
+            items={items}
+            chunkSize={chunkSize}
+            deleteItems={deleteItems}
+          />
           <Button
             className="ScrollTop self-center"
             type="button"
@@ -86,99 +89,3 @@ export const FeedCaughtUp: React.FC = () => (
     className="max-w-7xl"
   />
 );
-
-export function deleteableChunks(
-  items: ActivityFeedItem[],
-  chunkSize: number,
-  deleteItems: (ids: UUID[]) => unknown,
-): JSX.Element[] {
-  const ids: UUID[] = [];
-  const elements: JSX.Element[] = [];
-  const numChunks = Math.ceil(items.length / chunkSize);
-
-  for (let chunkIndex = 0; chunkIndex < numChunks; chunkIndex++) {
-    const chunkOffset = chunkIndex * chunkSize;
-    const chunkItems = items.slice(chunkOffset, chunkOffset + chunkSize);
-
-    let bufferingSuspensionItems = false;
-    let buffer: JSX.Element[] = [];
-
-    for (const item of chunkItems) {
-      const isLastItem = chunkItems.indexOf(item) === chunkItems.length - 1;
-      const finishingSuspension =
-        (!item.duringSuspension && bufferingSuspensionItems) ||
-        (isLastItem && item.duringSuspension);
-      bufferingSuspensionItems = item.duringSuspension;
-
-      if (item.duringSuspension) {
-        buffer.push(renderItem(item, () => deleteItems([item.id])));
-      }
-      if (finishingSuspension) {
-        elements.push(
-          <div
-            key={buffer[0]?.key}
-            className="ml-2 md:-ml-6 mt-4 pl-4 md:pl-5 rounded-l-3xl border-4 border-r-0 border-red-500/60"
-          >
-            <div className="bg-slate-100 md:bg-slate-50 -mt-4 pl-3 font-medium text-lg text-red-600">
-              During filter suspension
-            </div>
-            <div className="flex flex-col gap-8 pt-2 pb-4">{buffer}</div>
-            <div className="bg-slate-100 md:bg-slate-50 h-2 -mb-1 ml-8"></div>
-          </div>,
-        );
-        buffer = [];
-      }
-      if (!item.duringSuspension) {
-        elements.push(renderItem(item, () => deleteItems([item.id])));
-      }
-      ids.push(item.id);
-    }
-
-    if (chunkIndex < numChunks - 1) {
-      const toDelete = [...ids];
-      elements.push(
-        <div
-          key={`${ids[ids.length - 1] ?? ``}-separator`}
-          className="flex justify-center pb-8"
-        >
-          <Button
-            type="button"
-            color="secondary-on-violet-bg"
-            className="ScrollTop"
-            onClick={() => deleteItems(toDelete)}
-          >
-            Approve previous {[...ids].length} items
-          </Button>
-        </div>,
-      );
-    }
-  }
-  return elements;
-}
-
-function renderItem(item: ActivityFeedItem, deleteItem: () => unknown): JSX.Element {
-  if (item.type === `Screenshot`) {
-    return (
-      <ScreenshotViewer
-        key={item.id}
-        url={item.url}
-        width={item.width}
-        height={item.height}
-        onApprove={deleteItem}
-        date={new Date(item.date)}
-        duringSuspension={item.duringSuspension}
-      />
-    );
-  } else {
-    return (
-      <KeystrokesViewer
-        key={item.id}
-        strokes={item.line}
-        application={item.appName}
-        onApprove={deleteItem}
-        duringSuspension={item.duringSuspension}
-        date={new Date(item.date)}
-      />
-    );
-  }
-}
