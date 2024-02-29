d16545e3cd211d1606d8f56f8e7f2a7cc4493959
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-02-29 09:06:34 -0500

    site: removed unused font styles

--- site/app/docs/page.tsx
+++ site/app/docs/page.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const DocsHomePage: React.FC = () => (
-  <div>
-    <h1>Docs Home Page</h1>
-    <p>sweet this actually kinda works</p>
-  </div>
-);
+const DocsHomePage: React.FC = () => <div></div>;
 
 export default DocsHomePage;
