6f01b74d19bd5024623de427aec5c4994d40a809
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-21 14:09:47 -0400

    site/storybook: fix storybook errors, prep for vercel deploy

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -1,14 +1,6 @@
-import { mergeConfig } from 'vite';
+import { StorybookConfig } from '@storybook/nextjs';
 
 module.exports = {
-  async viteFinal(config, { configType }) {
-    return mergeConfig(config, {
-      // fix next/link issue with process.env not defined (build only)
-      // https://github.com/storybookjs/storybook/issues/18920#issuecomment-1310273755
-      // check if still necessary when upgrading to next 13
-      define: { 'process.env': {} },
-    });
-  },
   stories: [`../src/**/*.stories.@(ts|tsx)`],
   addons: [
     `@storybook/addon-links`,
@@ -16,9 +8,9 @@ module.exports = {
     `@storybook/addon-interactions`,
     `storybook-tailwind-dark-mode`,
   ],
-  framework: `@storybook/react-vite`,
+  framework: `@storybook/nextjs`,
   features: {
     buildStoriesJson: true,
   },
-  staticDirs: [`../../dash/app/public`],
+  staticDirs: [`../../dash/app/public`, `../../site/public`],
 };
