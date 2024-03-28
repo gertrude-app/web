d2f8eaf988d8ead117d44efa99496cc6021e019a
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-28 11:01:42 -0400

    wip: some more stuff

--- sb8/config/main.ts
+++ sb8/config/main.ts
@@ -1,19 +1,13 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: [`../src/**/*.mdx`, `../src/**/*.stories.@(js|jsx|mjs|ts|tsx)`],
+  stories: [`../src/**/*.stories.@(js|jsx|mjs|ts|tsx)`],
   addons: [
     `@storybook/addon-links`,
     `@storybook/addon-essentials`,
     `@storybook/addon-interactions`,
   ],
-  framework: {
-    name: `@storybook/nextjs`,
-    options: {},
-  },
-  docs: {
-    autodocs: `tag`,
-  },
-  staticDirs: [`../public`],
+  framework: `@storybook/nextjs`,
+  typescript: { reactDocgen: false },
 };
 export default config;
