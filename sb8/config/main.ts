b69ea862bacc942aed6e42c65d30dcfc5e4ef5b5
Jared Henderson <jaredh159@users.noreply.github.com>
2024-03-29 11:28:50 -0400

    wip: remove some more preinstalled addons

--- sb8/config/main.ts
+++ sb8/config/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [`../src/**/*.stories.@(js|jsx|mjs|ts|tsx)`],
-  addons: [
-    `@storybook/addon-links`,
-    `@storybook/addon-essentials`,
-    `@storybook/addon-interactions`,
-  ],
+  addons: [`@storybook/addon-essentials`],
   framework: `@storybook/nextjs`,
   typescript: { reactDocgen: false },
   staticDirs: [`../public`],
