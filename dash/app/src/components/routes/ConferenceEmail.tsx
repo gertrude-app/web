a558ee4ae73e441354e780c603e4103f2937a2c0
Jared Henderson <jaredh159@users.noreply.github.com>
2024-04-17 16:29:38 -0400

    dash: repurpose holland talk route for inch conf

--- dash/app/src/components/routes/ConferenceEmail.tsx
+++ dash/app/src/components/routes/ConferenceEmail.tsx
@@ -5,10 +5,10 @@ import { Button, Loading, Logo, TextInput } from '@shared/components';
 import { useMutation } from '../../hooks';
 import Current from '../../environment';
 
-const HollandTalk: React.FC = () => {
+const ConferenceEmailForm: React.FC<{ source: 'workshop' | 'booth' }> = ({ source }) => {
   const [email, setEmail] = React.useState(``);
   const [emailValid, setEmailValid] = React.useState(true);
-  const subscription = useMutation(() => Current.api.hollandTalkSubscription({ email }));
+  const submit = useMutation(() => Current.api.saveConferenceEmail({ email, source }));
 
   return (
     <FullscreenModalForm state="idle">
@@ -19,27 +19,27 @@ const HollandTalk: React.FC = () => {
             <span className="text-4xl font-lato">
               <span className="text-slate-800">Gertrude</span>
             </span>
-            <span className="bg-gradient-to-r from-indigo-500 to-fuchsia-600 bg-clip-text [-webkit-background-clip:text] text-transparent w-fit text-base uppercase font-semibold -mt-1">
-              Live in Holland, MI
+            <span className="bg-gradient-to-r from-indigo-500 to-fuchsia-600 bg-clip-text [-webkit-background-clip:text] text-transparent w-fit text-base uppercase font-semibold -mt-1 tracking-wide">
+              Live at INCH 2024
             </span>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center my-12">
-          <div className="text-2xl font-bold">Thank you for attending!</div>
-          <p className="text-lg text-slate-500 text-center mt-2 relative">
-            Enter your email address to be notified when we release the talk online.
-          </p>
-          <div className="self-stretch flex justify-end pr-8">
-            <span className="font-signature text-6xl -rotate-[20deg] text-fuchsia-500">
-              -Jared
-            </span>
+          <div className="text-2xl font-bold">
+            Thanks for {source === `workshop` ? `listening!` : `stopping by!`}
           </div>
+          <p className="text-lg text-slate-500 text-center mt-3 relative">
+            {source === `workshop`
+              ? `Enter your email address to be notified when we release the talk online.`
+              : `Enter your email address and we'll reach out soon to help you get started with Gertrude.`}
+          </p>
         </div>
         <TextInput
           type="email"
           value={email}
           setValue={setEmail}
-          placeholder="me@example.com"
+          autoFocus
+          placeholder="you@example.com"
         />
         <Button
           type="button"
@@ -49,7 +49,7 @@ const HollandTalk: React.FC = () => {
               return;
             }
             setEmailValid(true);
-            subscription.mutate(undefined);
+            submit.mutate(undefined);
           }}
           color="primary"
           size="large"
@@ -57,22 +57,22 @@ const HollandTalk: React.FC = () => {
         >
           Submit <i className="fas fa-arrow-right ml-2" />
         </Button>
-        {subscription.isPending && <Loading className="mt-6" />}
-        {!emailValid && subscription.isIdle && (
+        {submit.isPending && <Loading className="mt-6" />}
+        {!emailValid && submit.isIdle && (
           <Message
             heading="Invalid email address"
             message="Please enter a valid email address"
             success={false}
           />
         )}
-        {subscription.isError && (
+        {submit.isError && (
           <Message
             success={false}
             heading="Uh oh!"
             message="Looks like something went wrong. Please refresh the page and try again."
           />
         )}
-        {subscription.isSuccess && (
+        {submit.isSuccess && (
           <Message
             success={true}
             heading="Success!"
@@ -84,7 +84,7 @@ const HollandTalk: React.FC = () => {
   );
 };
 
-export default HollandTalk;
+export default ConferenceEmailForm;
 
 const Message: React.FC<{ heading: string; message: string; success: boolean }> = ({
   heading,
--- dash/types/src/pairql/pairs/SaveConferenceEmail.ts
+++ dash/types/src/pairql/pairs/SaveConferenceEmail.ts
@@ -1,9 +1,11 @@
 // auto-generated, do not edit
 import type { SuccessOutput } from '../shared';
 
-export namespace HollandTalkSubscription {
+export namespace SaveConferenceEmail {
   export interface Input {
     email: string;
+    source: 'workshop' | 'booth';
   }
+
   export type Output = SuccessOutput;
 }
