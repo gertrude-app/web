8ff546f7ffc121c8bff164a023a615e461fb1d7d
Jared Henderson <jaredh159@users.noreply.github.com>
2024-05-08 15:04:32 -0400

    tweak exodus-cry slide

--- site/app/pages/slideshow/BestPornFilter.tsx
+++ site/app/pages/slideshow/BestPornFilter.tsx
@@ -6,13 +6,20 @@ export const Slide: React.FC = () => (
     <h1 className="font-mono mt-[440px] font-bold text-[9.3rem] text-center text-black">
       #1 Rated Internet Filter
     </h1>
-    <img src={Logo.src} className="w-[1200px] absolute top-[-0px] left-[1124px]" alt="" />
+    <img
+      src={Logo.src}
+      className="w-[1200px] opacity-75 absolute top-[-0px] left-[1124px]"
+      alt=""
+    />
+    <p className="text-white text-[5.8rem] opacity-75 font-mono absolute top-[20rem] right-[70rem] *opacity-75 tracking-[-10px]">
+      &rsquo;s
+    </p>
     <div className="font-serif relative ml-[1000px] leading-[1.25em] text-center text-7xl max-w-[1400px] mt-36 text-white">
       <div className="font-serif absolute left-[-180px] top-[100px] text-white text-[500px]">
         &ldquo;
       </div>
-      The only filtering software which lets you block the entire internet, only unlocking
-      the portions you choose.
+      Gertrude is the only filtering software which lets you block the entire internet,
+      only unlocking the portions you choose.
       <div className="font-serif absolute text-white text-[500px] bottom-[-95px] right-[-210px]">
         &rdquo;
       </div>
