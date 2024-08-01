344381fa26d2242671c09898ec181d77406a0a76
Jared Henderson <jaredh159@users.noreply.github.com>
2024-08-01 16:22:58 -0400

    dash: unstyled, ulinked security events feed

---
+++ dash/types/src/pairql/pairs/SecurityEventsFeed.ts
@@ -0,0 +1,27 @@
+// auto-generated, do not edit
+
+export namespace SecurityEventsFeed {
+  export type Input = void;
+
+  export type Output = Array<
+    | {
+        case: 'child';
+        id: UUID;
+        childId: UUID;
+        deviceId: UUID;
+        event: string;
+        detail?: string;
+        explanation: string;
+        createdAt: ISODateString;
+      }
+    | {
+        case: 'admin';
+        id: UUID;
+        event: string;
+        detail?: string;
+        explanation: string;
+        ipAddress?: string;
+        createdAt: ISODateString;
+      }
+  >;
+}
