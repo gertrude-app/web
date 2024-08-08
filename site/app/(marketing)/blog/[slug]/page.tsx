6502ec90e513bbda2d38b54da927719e4302c0ff
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-08-08 10:41:37 -0400

    site: added cta to blog articles

--- site/app/(marketing)/blog/[slug]/page.tsx
+++ site/app/(marketing)/blog/[slug]/page.tsx
@@ -1,14 +1,16 @@
 import path from 'path';
 import React from 'react';
-import { ChevronLeftIcon } from 'lucide-react';
+import { ArrowRightIcon, ChevronLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 import cx from 'classnames';
 import Markdoc from '@markdoc/markdoc';
+import { Logo } from '@shared/components';
 import type { Metadata, NextPage } from 'next';
 import { getArticle, getArticlePaths } from '@/markdoc/files';
 import { components } from '@/markdoc/config';
 import Prose from '@/components/articles/Prose';
 import { createMetadata } from '@/lib/seo';
+import FancyLink from '@/components/FancyLink';
 
 type Params = {
   slug: string;
@@ -58,6 +60,27 @@ const BlogArticle: NextPage<PageProps> = async ({ params: { slug } }) => {
         </span>
         <h1 className="text-4xl font-bold mb-8 mt-2 break-words">{title}</h1>
         <Prose>{Markdoc.renderers.react(content, React, { components })}</Prose>
+        <div className="mt-16 p-8 sm:p-12 bg-gradient-to-br from-violet-500 to-fuchsia-500 rounded-3xl flex flex-col items-center">
+          <Link href="/">
+            <Logo type="inverted" />
+          </Link>
+          <p className="mt-8 text-center text-white sm:text-lg">
+            The Gertrude mac app helps you <b>protect your kids</b> online with{` `}
+            <b>strict internet filtering</b> that you can manage from your{` `}
+            <b>own computer or phone,</b> plus remote monitoring of screenshots and
+            keylogging. $5/mo, with a 60 day free trial.
+          </p>
+          <FancyLink
+            type="link"
+            href="https://parents.gertrude.app/signup"
+            color="primary"
+            inverted
+            className="mt-8"
+            Icon={ArrowRightIcon}
+          >
+            Start free trial
+          </FancyLink>
+        </div>
       </div>
     </div>
   );
