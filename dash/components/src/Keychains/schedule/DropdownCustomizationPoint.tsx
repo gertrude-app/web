013f55c6a470a7b302a76445b7a92441f3c657aa
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-10-01 10:23:59 -0400

    dash: made scheduling ui responsive

--- dash/components/src/Keychains/schedule/DropdownCustomizationPoint.tsx
+++ dash/components/src/Keychains/schedule/DropdownCustomizationPoint.tsx
@@ -5,11 +5,13 @@ import { ChevronDownIcon } from '@heroicons/react/24/outline';
 interface DropdownCustomizationPointProps {
   text: React.ReactNode;
   children: React.ReactNode;
+  isTouchDevice: boolean;
 }
 
 const DropdownCustomizationPoint: React.FC<DropdownCustomizationPointProps> = ({
   text,
   children,
+  isTouchDevice,
 }) => {
   const [open, setOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -41,8 +43,12 @@ const DropdownCustomizationPoint: React.FC<DropdownCustomizationPointProps> = ({
         className={cx(
           `font-medium flex items-center group py-0.5 rounded-lg active:scale-90 active:bg-slate-300 transition-[transform,background-color,margin,padding,box-shadow] duration-200 relative`,
           open
-            ? `px-2 bg-white mx-4 shadow-md`
-            : `px-[3px] hover:px-2 hover:bg-slate-200`,
+            ? `px-2 bg-white shadow-md`
+            : isTouchDevice
+              ? `px-2 bg-slate-200/60`
+              : `px-2 bg-slate-200/60 hover:bg-slate-200 min-[672px]:px-[3px] min-[672px]:hover:px-2 min-[672px]:bg-transparent min-[672px]:hover:bg-slate-200`,
+
+          open && !isTouchDevice && `md:mx-4`,
         )}
         onClick={() => setOpen(!open)}
       >
@@ -50,7 +56,11 @@ const DropdownCustomizationPoint: React.FC<DropdownCustomizationPointProps> = ({
         <ChevronDownIcon
           className={cx(
             `shrink-0 transition-[width,margin-left,filter,transform] text-slate-400 duration-200`,
-            open ? `w-4 ml-2 -rotate-180` : `w-0 ml-0 group-hover:w-4 group-hover:ml-2`,
+            open
+              ? `w-4 ml-2 -rotate-180`
+              : isTouchDevice
+                ? `w-4 ml-2`
+                : `w-4 ml-2 min-[672px]:w-0 min-[672px]:ml-0 min-[672px]:group-hover:w-4 min-[672px]:group-hover:ml-2`,
           )}
           strokeWidth={2.5}
         />
