ffe5f78f632cd4fe596fdd241a77b56e676e8f86
Jared Henderson <jaredh159@users.noreply.github.com>
2024-11-13 16:17:43 -0500

    appviews: compat fixes for catalina 2.5.0 ui changes

--- appviews/src/MenuBar/states/connected/PauseDowntimeModal.tsx
+++ appviews/src/MenuBar/states/connected/PauseDowntimeModal.tsx
@@ -13,7 +13,7 @@ const PauseDowntimeModal: React.FC<Props> = ({ open, emit, dispatch }) => (
   <>
     <div
       className={cx(
-        `bg-white/50 dark:bg-white/10 backdrop-blur shadow-lg absolute z-20 px-3 py-2 rounded-xl w-80 transition-[opacity,transform] duration-300`,
+        `bg-white/50 dark:bg-white/10 shadow-lg absolute z-20 px-3 py-2 rounded-xl w-80 transition-[opacity,transform] duration-300`,
         !open && `pointer-events-none opacity-0 translate-y-32 scale-0 -translate-x-32`,
       )}
     >
@@ -26,18 +26,21 @@ const PauseDowntimeModal: React.FC<Props> = ({ open, emit, dispatch }) => (
           <i className="fa fa-times text-black/40 dark:text-white/70 text-sm bg-black/10 dark:bg-white/10 w-5 h-5 rounded-full flex justify-center items-center hover:bg-black/20 dark:hover:bg-white/20 hover:scale-110 active:bg-black/30 dark:active:bg-white/30 active:scale-95 transition-[background-color,transform] duration-200" />
         </button>
       </header>
-      <div className="flex flex-wrap justify-center gap-2 py-4">
+      <div className="flex flex-wrap justify-center py-4">
         <Btn.White
+          className="mx-1 mb-2"
           onClick={() => emit({ case: `pauseDowntimeClicked`, duration: `tenMinutes` })}
         >
           10 minutes
         </Btn.White>
         <Btn.White
+          className="mx-1 mb-2"
           onClick={() => emit({ case: `pauseDowntimeClicked`, duration: `oneHour` })}
         >
           1 hour
         </Btn.White>
         <Btn.White
+          className="mx-1 mb-2"
           onClick={() => emit({ case: `pauseDowntimeClicked`, duration: `oneDay` })}
         >
           Until next downtime
@@ -46,7 +49,7 @@ const PauseDowntimeModal: React.FC<Props> = ({ open, emit, dispatch }) => (
     </div>
     <div
       className={cx(
-        `absolute w-full h-full top-0 left-0 z-10 transition-colors, duration-300`,
+        `absolute w-full h-full top-0 left-0 bottom-0 right-0 z-10 transition-colors, duration-300`,
         open ? `bg-black/[15%]` : `bg-transparent pointer-events-none`,
       )}
       onClick={() => dispatch({ type: `toggleShowingDowntimePauseDuration` })}
