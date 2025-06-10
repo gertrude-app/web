56cebc8bd0e7875c5da4c712e0489a32fec2c3fa
Jared Henderson <jaredh159@users.noreply.github.com>
2025-06-10 14:48:44 -0400

    monorepo: update to node 22, pnpm 10

--- dash/app/tailwind.config.cjs
+++ dash/app/tailwind.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   presets: [require(`@shared/tailwind`)],
   content: [
     `./src/components/**/*.tsx`,
-    `../components/**/*.tsx`,
+    `../components/src/**/*.tsx`,
     `../../shared/components/**/*.tsx`,
   ],
 };
